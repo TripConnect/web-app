541b2833fa0fc39e6f736b8351c96238b33b6ef4
ndtrong <nguyenductrong12a1@gmail.com>
2025-02-08 23:04:12 +0700

    Update: Store lives socket with useState instead of useRef

--- src/pages/LivestreamHost.tsx
+++ src/pages/LivestreamHost.tsx
@@ -11,80 +11,81 @@ export default function LivestreamHost() {
     const currentUser = useSelector((state: RootState) => state.user);
     const videoRef = useRef<HTMLVideoElement | null>(null);
     const recorderRef = useRef<MediaRecorder | null>(null);
-    const livesConnRef = useRef<Socket | null>(null);
+    const [livesSocket, setLivesSocket] = useState<Socket | null>(null);
     const mediaStreamRef = useRef<MediaStream | null>(null);
 
     useEffect(() => {
         console.log('Initiating Livestream resources');
-        livesConnRef.current = io(`${process.env.REACT_APP_BASE_URL}/livestream`, {
+        const livestreamSocket = io(`${process.env.REACT_APP_BASE_URL}/livestream`, {
             transports: ['websocket'],
             auth: {
                 token: currentUser.accessToken
             }
-        })
-            .on('connect', () => {
-                setTimeout(async () => {
-                    console.log('Livestream socket connected');
-                    if (!mediaStreamRef.current) {
-                        console.log('Apply media stream');
-                        mediaStreamRef.current = await navigator.mediaDevices.getUserMedia(mediaConstraints);
-                    }
+        });
+        livestreamSocket.on('connect', () => {
+            setTimeout(async () => {
+                console.log('Livestream socket connected');
+                if (!mediaStreamRef.current) {
+                    console.log('Apply media stream');
+                    mediaStreamRef.current = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+                }
 
-                    if (videoRef.current && !videoRef.current?.srcObject) {
-                        console.log('Apply video source');
-                        videoRef.current.srcObject = mediaStreamRef.current;
-                        livesConnRef.current?.emit(
-                            "start",
-                            { roomId },
-                            (ack: { status: 'SUCCESS' | 'FAILED' }) => {
-                                console.log("Start livestream: " + ack.status);
-                            }
-                        );
-                    }
+                if (videoRef.current && !videoRef.current?.srcObject) {
+                    console.log('Apply video source');
+                    videoRef.current.srcObject = mediaStreamRef.current;
+                    livestreamSocket?.emit(
+                        "start",
+                        { roomId },
+                        (ack: { status: 'SUCCESS' | 'FAILED' }) => {
+                            console.log("Start livestream: " + ack.status);
+                        }
+                    );
+                }
 
-                    if (!recorderRef.current) {
-                        console.log('Apply video recorder');
-                        recorderRef.current = new MediaRecorder(mediaStreamRef.current);
-                        recorderRef.current.addEventListener('dataavailable', event => {
-                            console.log('Recorder data available');
-                            if (event.data.size > 0) {
-                                console.log('Send segment');
-                                livesConnRef.current?.emit(
-                                    'segment',
-                                    {
-                                        roomId,
-                                        segment: event.data
-                                    },
-                                    (ack: { status: 'SUCCESS' | 'FAILED' }) => {
-                                        debugger
-                                        console.log("Record: " + ack.status);
-                                    });
-                            }
-                        });
-                        recorderRef.current.addEventListener('error', event => {
-                            console.error(event);
-                        });
-                        recorderRef.current.addEventListener('stop', event => {
-                            console.warn('Video recorder stopped');
-                        });
-                        recorderRef.current.start(5_000);
-                    }
-                }, 1000);
-            })
-            .on('disconnect', () => {
-                console.log('Livestream socket disconnected');
-            })
-            .on('connect_error', async (error) => {
-                console.error(error);
-            });
+                if (!recorderRef.current) {
+                    console.log('Apply video recorder');
+                    recorderRef.current = new MediaRecorder(mediaStreamRef.current);
+                    recorderRef.current.addEventListener('dataavailable', event => {
+                        console.log('Recorder data available');
+                        if (event.data.size > 0) {
+                            console.log('Send segment');
+                            livestreamSocket?.emit(
+                                'segment',
+                                {
+                                    roomId,
+                                    segment: event.data
+                                },
+                                (ack: { status: 'SUCCESS' | 'FAILED' }) => {
+                                    console.log("Record: " + ack.status);
+                                }
+                            );
+                        }
+                    });
+                    recorderRef.current.addEventListener('error', event => {
+                        console.error(event);
+                    });
+                    recorderRef.current.addEventListener('stop', event => {
+                        console.warn('Video recorder stopped');
+                    });
+                    recorderRef.current.start(5_000);
+                }
+            }, 1000);
+        });
+        livestreamSocket.on('disconnect', () => {
+            console.log('Livestream socket disconnected');
+        });
+        livestreamSocket.on('connect_error', async (error) => {
+            console.error(error);
+        });
+
+        setLivesSocket(livestreamSocket);
 
         return () => {
             console.log('Clean up useEffect resources');
-            livesConnRef.current?.disconnect();
+            livestreamSocket?.disconnect();
             recorderRef.current?.stop();
             mediaStreamRef.current?.getTracks().forEach(track => track.stop());
 
-            livesConnRef.current = null;
             recorderRef.current = null;
             mediaStreamRef.current = null;
         };
