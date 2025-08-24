084b039af2ed6264095b377aba214dc655012ad1
ndtrong <nguyenductrong12a1@gmail.com>
2025-08-24 11:18:30 +0700

    feat: implement header search bar

--- src/shared/components/Header/components/SearchBar/index.tsx
+++ src/shared/components/Header/components/SearchBar/index.tsx
@@ -1,8 +1,17 @@
-import {TextField} from "@mui/material";
+import {InputAdornment, TextField} from "@mui/material";
+import {Search} from "@mui/icons-material";
 
-export default function () {
+export default function SearchBar() {
   return (
     <TextField
+      placeholder={"Search users, destinations, travelers, trips..."}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Search/>
+          </InputAdornment>
+        ),
+      }}
       fullWidth
     />
   )
