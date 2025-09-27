db45fdb901b70d318bd9e6a109a6a6fbe688a0b8
ndtrong <nguyenductrong12a1@gmail.com>
2025-09-27 21:39:26 +0700

    style: refactor livestream preview ui

--- src/pages/livestream/components/LivePreview.tsx
+++ src/pages/livestream/components/LivePreview.tsx
@@ -1,4 +1,4 @@
-import {Avatar, Badge, Box, Card, CardContent, CardMedia, Chip, Typography} from "@mui/material";
+import {Avatar, Box, Card, CardContent, CardMedia, Chip, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../store";
@@ -26,8 +26,9 @@ export default function LivePreview(props: Props) {
       <Chip icon={<SensorsIcon fontSize='small'/>} label={t('LIVESTREAM.LIVE')} color='error'
             style={{
               position: "absolute",
-              top: 238,
+              top: 242,
               right: 10,
+              height: 28,
               borderRadius: 4,
               textTransform: 'uppercase',
               transform: 'scale(0.95)',
@@ -41,34 +42,31 @@ export default function LivePreview(props: Props) {
       />
       <CardContent style={{padding: 10, paddingLeft: 0}}>
         <Box display='flex' justifyContent='start' alignItems='start' gap={1.2}>
-          <Box sx={{position: 'relative', display: 'inline-flex'}}>
-            <Badge
-              badgeContent={
-                <Chip
-                  size="small"
-                  icon={<SensorsIcon sx={{fontSize: '1rem', transform: 'translateX(25%) scale(1.2)'}}/>}
-                  color="error"
-                  sx={{
-                    borderRadius: '4px',
-                    transform: 'translate(60%, -40%) scale(0.55)',
-                    height: 'auto',
-                    padding: '2px',
-                  }}
-                />
-              }
-              anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
-              sx={{'--badge-color': '#e1002d'}}
-            >
-              <Avatar
-                src={currentUser.avatar}
-                sx={{
-                  objectFit: 'cover',
-                  width: 46,
-                  height: 46,
-                  border: '4px solid var(--badge-color)',
-                }}
-              />
-            </Badge>
+          <Box position='relative' width={46} height={46} sx={{'--badge-color': '#e1002d'}}>
+            <Avatar
+              src={currentUser.avatar}
+              sx={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                border: '4px solid var(--badge-color)',
+                objectFit: 'cover',
+              }}
+            />
+            <Chip
+              size="small"
+              icon={<SensorsIcon sx={{fontSize: '1rem', transform: 'translateX(25%) scale(1.2)'}}/>}
+              color="error"
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                borderRadius: '4px',
+                transform: 'translate(10%, 10%) scale(0.55)',
+                height: 'auto',
+                padding: '2px',
+              }}
+            />
           </Box>
           <Box display='flex' flexDirection={'column'} justifyContent='center' alignItems='start'>
             <Typography variant={"h6"} title={title} maxWidth={400} whiteSpace={'nowrap'} overflow={'hidden'}
